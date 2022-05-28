81ab27523317f3fd5f087e1ced2fd95a8f836b80
Anderson Buffon <abuffon@gmail.com>
2022-05-27 22:17:12 -0300

    Implemented client streaming

--- go-aluno/cmd/client/client.go
+++ go-aluno/cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/abuffon/fullcycle-grpc/pb"
 	"google.golang.org/grpc"
@@ -19,7 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	//AddUser(client)
-	AddUserVerbose(client)
+	//AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -60,4 +62,51 @@ func AddUserVerbose(client pb.UserServiceClient) {
 
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "a1",
+			Name:  "Anderson 1",
+			Email: "email1@email",
+		},
+		&pb.User{
+			Id:    "a2",
+			Name:  "Anderson 2",
+			Email: "email2@email",
+		},
+		&pb.User{
+			Id:    "a3",
+			Name:  "Anderson 3",
+			Email: "email3@email",
+		},
+		&pb.User{
+			Id:    "a4",
+			Name:  "Anderson 4",
+			Email: "email4@email",
+		},
+		&pb.User{
+			Id:    "a5",
+			Name:  "Anderson 5",
+			Email: "email5@email",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
 }
